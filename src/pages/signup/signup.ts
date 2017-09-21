1e3acafb6e24b4c93d40da008a7b754c9c85eea4
DrewGamble <s3544775@student.rmit.edu.au>
2017-09-21 15:38:17 +1000

    Validation for signup completed. Now just waiting to do POST pushing of form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -20,18 +20,18 @@ export class SignupPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.signupForm = formBuilder.group({
-      firstName: ["", Validators.compose([Validators.maxLength(90), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-      lastName: [""],
-      email: [""],
-      password: [""],
-      dob: [""],
-      licence: [""],
-      phonenumber: [""],
+      firstName: ["", Validators.compose([Validators.maxLength(60), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ["", Validators.compose([Validators.maxLength(60), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ["", Validators.compose([Validators.maxLength(255), Validators.pattern("^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$"), Validators.required])],
+      password: ["", Validators.compose([Validators.minLength(7), Validators.maxLength(255), Validators.required])],
+      dob: ["", Validators.compose([Validators.required])],
+      licence: ["", Validators.compose([Validators.minLength(5), Validators.maxLength(20), Validators.required])],
+      phone: ["", Validators.compose([Validators.minLength(8), Validators.maxLength(15), Validators.pattern('[+0-9 ]*'), Validators.required])],
       address1: [""],
       address2: [""],
       suburb: [""],
       state: [""],
-      postcode: [""]
+      postcode: ["", Validators.compose([Validators.minLength(4), Validators.maxLength(4), Validators.pattern('[0-9 ]*')])]
     })
   }
 
