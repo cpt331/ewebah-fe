fd83651dd90f5415e4014955e7d173b6da3cf0cd
DrewGamble <s3544775@student.rmit.edu.au>
2017-10-02 16:09:48 +1000

    Added homepage for Website  and added pages for the additional functionalities in the future sprints.
    Could drop the pages if we choose to use a different approach however.

---
+++ src/pages/return/return.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-return',
+  templateUrl: 'return.html'
+})
+export class ReturnPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
