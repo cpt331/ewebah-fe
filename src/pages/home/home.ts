beb775c4f8feba4b4ab9b701a3893e310b3918cd
Michael Davidson <s3353147@student.rmit.edu.au>
2017-10-07 15:59:01 +1100

    added loading map
    loads the map and centres in to user's locations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController, App } from 'ionic-angular';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { NavController, App} from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import {Geolocation} from '@ionic-native/geolocation';
+import {MarkerOptions,Marker} from '@ionic-native/google-maps';
+
 
 import { ReturnPage } from '../return/return';
 import { SettingsPage } from '../settings/settings';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -12,15 +17,40 @@ import { SettingsPage } from '../settings/settings';
 export class HomePage {
 
   userPostData = {"Name":"","Token":"","Email":""};
+  //Map stuff
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  
 
-  constructor(public navCtrl: NavController, public app: App) {
+  constructor(public navCtrl: NavController, public app: App, public geolocation: Geolocation) {
 
     const data = JSON.parse(localStorage.getItem('userData'));
   
     this.userPostData.Name = data.Name;
     this.userPostData.Email = data.Email;
     this.userPostData.Token = data.access_token;
+  
+         
+  }
+    //more map stuff
+  ionViewDidLoad() {
+      this.loadMap();
+  }
 
+
+  loadMap() {
+    //get user location
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng= new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    //set map options
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: 'roadmap'
+      }
+      
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+      });
   }
 
   book()
