db4fefc50c5a6f2f25d27f8947a0a2fb81b75c9e
Steven Innes <steven.innes@gmail.com>
2017-10-21 15:23:00 +1100

    added admin home page and cars listing

---
+++ src/pages/admin-car/admin-car.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { AdminCarsPage } from '../admin-cars/admin-cars';
+/**
+ * Generated class for the AdminCarPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-admin-car',
+  templateUrl: 'admin-car.html',
+})
+export class AdminCarPage {
+
+  private carForm : FormGroup;
+  private car = null;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder ) {
+    
+    this.car = this.navParams.get('car');
+    
+    this.carForm = this.formBuilder.group({
+      Make: [this.car.Make, Validators.required],
+      Model: [this.car.Model, Validators.required],
+    });
+
+
+  }
+  submitForm(){
+    console.log('submit!');
+    this.navCtrl.push(AdminCarsPage, {}, {animate: true});
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AdminCarPage');
+  }
+
+}
