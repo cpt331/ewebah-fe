433eac41261cc84d340b908523249f0dad836bfb
DrewGamble <s3544775@student.rmit.edu.au>
2017-10-24 15:03:32 +1000

    Update to fix services
    updated api links

--- src/providers/car-service/car-service.ts
+++ src/providers/car-service/car-service.ts
@@ -3,7 +3,7 @@ import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 // let apiUrl = 'http://careshareapi-env.hdwwh7zgb3.us-east-1.elasticbeanstalk.com/';
-let apiUrl = 'http://carshareapi-dev.us-east-1.elasticbeanstalk.com/';
+let apiUrl = 'https://ewebahapi.azurewebsites.net/';
 
 
 @Injectable()
