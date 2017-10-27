b8f4400a56dac27d7c3014d2875d36a088ff8833
Steven Innes <steven.innes@gmail.com>
2017-10-28 09:19:33 +1100

    added new services and admin features to car service and admin car page

--- src/pages/admin-car/admin-car.ts
+++ src/pages/admin-car/admin-car.ts
@@ -3,6 +3,12 @@ import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { AdminCarsPage } from '../admin-cars/admin-cars';
+import { CarServiceProvider } from '../../providers/car-service/car-service'
+import { UpdateCarRequest } from '../../providers/car-service/update-car-request';
+import { UpdateCarResponse } from '../../providers/car-service/update-car-response';
+import { CarCategory } from '../../providers/car-service/car-category';
+import { City } from '../../providers/car-service/city';
+
 /**
  * Generated class for the AdminCarPage page.
  *
@@ -18,32 +24,141 @@ import { AdminCarsPage } from '../admin-cars/admin-cars';
 export class AdminCarPage {
 
   private carForm : FormGroup;
+  private formSubmitting : boolean = false;
   private car = null;
+  private currentUser = {access_token: "", Name: "",Email: "",Id: "", token_type:"",HasOpenBooking: false, OpenBookingId:-1};
+  private updateCarResponse : UpdateCarResponse = null;
+  private updateInProgress : boolean = false;
+  private categoriesLoading : boolean = true;
+  private categories : CarCategory[] = null;
+  private statusesLoading : boolean = true;
+  private statuses : string[] = null;
+  private citiesLoading = true;
+  private cities : City[] = null;
+  
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder , public carService: CarServiceProvider) {
     
     this.car = this.navParams.get('car');
 
     if(!this.car){
       this.car = {
+          Id: null,
           Make: '',
-          Model: ''
+          Model: '',
+          CarCategory: null,
+          Transmission: null,
+          Suburb: '',
+          Status: '',
+          LatPos: '',
+          LongPos: ''
       };
     }
     
     this.carForm = this.formBuilder.group({
       Make: [this.car.Make, Validators.required],
       Model: [this.car.Model, Validators.required],
+      CarCategory: [this.car.CarCategory, Validators.required],
+      Transmission: [this.car.Transmission, Validators.required],
+      Suburb: [this.car.Suburb, Validators.required],
+      Status: [this.car.Status, Validators.required],
+      LatPos: [this.car.LatPos, Validators.required],
+      LongPos: [this.car.LongPos, Validators.required]
     });
 
 
   }
-  submitForm(){
-    console.log('submit!');
-    this.navCtrl.push(AdminCarsPage, {}, {animate: true});
-  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdminCarPage');
+    this.loadUserData();
+    this.loadCategories();
+    this.loadStatuses();
+    this.loadCities();
+  }
+
+  loadCities()  {
+    let subscription = this.carService.getCities(this.currentUser.access_token)
+    .subscribe(
+    value => this.cities = value,
+    error => this.cities = null,
+    () => this.citiesLoading = false
+  );
+  }
+
+  loadCategories()  {
+    let subscription = this.carService.getCategories(this.currentUser.access_token)
+    .subscribe(
+    value => this.categories = value,
+    error => this.categories = null,
+    () => this.categoriesLoading = false
+  );
+  }
+
+  loadStatuses()  {
+    let subscription = this.carService.getStatuses(this.currentUser.access_token)
+    .subscribe(
+    value => this.statuses = value,
+    error => this.statuses = null,
+    () => this.statusesLoading = false
+  );
+  }
+
+  submitForm(){
+    console.log('submit!');
+    console.log(this.carForm.value.Make);
+    console.log(this.carForm.value.Model);
+
+    console.log(this.carForm);
+
+    this.updateInProgress = true;
+
+    let request = new UpdateCarRequest();
+
+    if(this.car.Id != null){
+      request.Id = this.car.Id;
+    }
+
+    request.Make = this.carForm.value.Make;
+    request.Model = this.carForm.value.Model;
+    request.CarCategory = this.carForm.value.CarCategory;
+    request.Transmission = this.carForm.value.Transmission;
+    request.Suburb = this.carForm.value.Suburb;
+    request.Status = this.carForm.value.Status;
+    request.LatPos = this.carForm.value.LatPos;
+    request.LongPos = this.carForm.value.LongPos;
+
+    let subscription = this.carService.updateCar(request, this.currentUser.access_token)
+          .subscribe(
+          value => function(value){
+
+            this.updateCarResponse = value;
+            this.processResponse(value);
+
+          },
+          error => function(error){
+            this.updateCarResponse = null
+          },
+          () => this.updateInProgress = false
+        );
+
+    //this.navCtrl.push(AdminCarsPage, {}, {animate: true});
+  
+  }
+  processResponse(response){
+
+  }
+
+  loadUserData(){
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.currentUser.Name = data.Name;
+    this.currentUser.Email = data.Email;
+    this.currentUser.access_token = data.access_token;
+    this.currentUser.token_type = data.token_type
+    this.currentUser.Id = data.Id
+    this.currentUser.HasOpenBooking = data.HasOpenBooking;
+    this.currentUser.OpenBookingId = data.OpenBookingId;
   }
 
 }
--- src/providers/car-service/car-service.ts
+++ src/providers/car-service/car-service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import * as Constants from '../providerConstants';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/observable';
+
+import { UpdateCarRequest } from './update-car-request';
+import { UpdateCarResponse } from './update-car-response';
+import { CarCategory } from './car-category';
+import { City } from './city';
 
 // let apiUrl = 'http://careshareapi-env.hdwwh7zgb3.us-east-1.elasticbeanstalk.com/';
 let apiUrl = Constants.API_ENDPOINT
@@ -14,6 +20,7 @@ export class CarServiceProvider {
   }
 
 
+  //return a list of all cars in the system no matter the status
   getAllCars(token) {
     return new Promise((resolve, reject) => {
       let headers = new Headers();
@@ -33,16 +40,73 @@ export class CarServiceProvider {
 
   }
 
-  updateCar(car, token){
+  //update a cars details with the supplied information
+  updateCar(request: UpdateCarRequest, token: string) : Observable<UpdateCarResponse> {
+        console.log('carService: updateCar(${request})');
+    
+            let headers: Headers = new Headers();
+            headers.append('accept','application/json');
+            headers.append('content-Type', 'application/json');
+            headers.append('authorization','Bearer ' + token);
+    
+            return this.http.post(
+                apiUrl +'api/cars/update', 
+                request, 
+                {headers:headers})
+            .map(response=> response.json());
+  }
 
-  };
 
-  deleteCar(vehicleId, token){
+  //return a list of available car categories for a car
+  getCategories(token : string) : Observable<CarCategory[]>{
+    
+    console.log('carService: getCategories()');
 
-  };
+        let headers: Headers = new Headers();
+        headers.append('accept','application/json');
+        headers.append('content-Type', 'application/json');
+        headers.append('authorization','Bearer ' + token);
+    
+        return this.http.get(
+            apiUrl +'api/cars/categories', 
+            {headers:headers})
+        .map(response=> response.json());
+        
+  }
 
-  getCategories(token){
+  //returns a list of available cities a car can be assigned to
+  getCities(token : string) : Observable<City[]>{
+    
+    console.log('carService: getCities()');
 
-  };
+        let headers: Headers = new Headers();
+        headers.append('accept','application/json');
+        headers.append('content-Type', 'application/json');
+        headers.append('authorization','Bearer ' + token);
+    
+        return this.http.get(
+            apiUrl +'api/cities', 
+            {headers:headers})
+        .map(response=> response.json());
+        
+  }
+
+  //returns a list of available statuses that can be assigned to a car
+  getStatuses(token : string) : Observable<string[]>{
+    
+    console.log('carService: getStatuses()');
+
+        let headers: Headers = new Headers();
+        headers.append('accept','application/json');
+        headers.append('content-Type', 'application/json');
+        headers.append('authorization','Bearer ' + token);
+    
+        return this.http.get(
+            apiUrl +'api/cars/statuses', 
+            {headers:headers})
+        .map(response=> response.json());
+        
+  }
+      
 
 }
