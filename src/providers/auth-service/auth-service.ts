fe119aed9e606c6627558f66d11ad1c934282fc8
chris <chrisespie@hotmail.com>
2017-10-29 12:15:16 +0800

    not worth commiting but need to update

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -182,7 +182,7 @@ export class AuthServiceProvider {
     
 
     //console.log(ccYear);
-      this.http.post(apiUrl + 'api/account/registerupdatereturn',userupdateRequest,
+      this.http.post(apiUrl + 'api/account/registerupdate',userupdateRequest,
           { headers: headers })
         //{ headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
 
